4e112a52b015f2379b751fdcf8f7a1c0f29ed9fb
Syed Humza <105381562+syedhumzasalman@users.noreply.github.com>
2025-02-16 18:14:15 +0500

    5

--- JS/startnow.js
+++ JS/startnow.js
@@ -54,7 +54,27 @@ body.appendChild(image);
 
 setTimeout(function() {
     body.removeChild(image);
-}, 4000);
+}, 3000);
+
+// ***********************************************************************//
+
+body = document.body;
+
+let video = document.createElement("video");
+
+video.src = "Videos/startnow-background.mp4";
+video.autoplay = true;
+video.loop = true;
+video.muted = true;
+video.style.position = "fixed";
+video.style.top = "0";
+video.style.left = "0";
+video.style.width = "100vw";
+video.style.height = "100vh";
+video.style.objectFit = "cover";
+video.style.zIndex = "-1";
+
+body.appendChild(video);
 
 
 
@@ -62,48 +82,79 @@ setTimeout(function() {
 
 // *******************************************************************//
 
-
-
+setTimeout(() => {
+    
 let characters = {
-    image1: "https://www.fightersgeneration.com/nz9/char/cfe/alex-capcom-fighting-allstars-face.png",
+    image1: "https://www.fightersgeneration.com/characters/alex-shirtrip.gif",
     image2: "https://www.fightersgeneration.com/characters/raidengif.gif",
     image3: "https://www.fightersgeneration.com/characters5/blake-belladonna-artwork_small.png",
     image4: "https://www.fightersgeneration.com/nz7/char/breakers/condor-breakers-revenge-art3.png",
-    image5: "https://www.fightersgeneration.com/characters/raidengif.gif",
-    image6: "https://www.fightersgeneration.com/characters/raidengif.gif",
-    image7: "https://www.fightersgeneration.com/characters/raidengif.gif",
-    image8: "https://www.fightersgeneration.com/characters/raidengif.gif",
-    image9: "https://www.fightersgeneration.com/characters/raidengif.gif",
-    image9: "https://www.fightersgeneration.com/characters/raidengif.gif",
-    image9: "https://www.fightersgeneration.com/characters/raidengif.gif",
-    image10: "https://www.fightersgeneration.com/characters/raidengif.gif",
+    image5: "https://www.fightersgeneration.com/characters4/chung-kiz_small.jpg",
+    image6: "https://www.fightersgeneration.com/characters3/spidey-whateva.gif",
+    image7: "https://www.fightersgeneration.com/characters4/the-flash-puzzle-and-dragons-artwork_small.jpg",
+    image8: "https://www.fightersgeneration.com/characters/venom-mc.gif",
+    image9: "https://www.fightersgeneration.com/characters4/white-realbout-dominantedmind-pissed_small.png",
+    image10: "https://www.fightersgeneration.com/characters2/jill-bigshinkiro_small.jpg",
 }
 
 
 
 let container = document.getElementById("boxContainer");
 
+// idhar ik loop chal raha he phale wale boxContainer pe
+
 for (let key in characters) {
     let box = document.createElement("div");
     box.classList.add("box");
-
+    
     let img = document.createElement("img");
     img.src = characters[key];
+    
+    img.addEventListener("click", function(){
+        let bigCharone = document.getElementById("bigCharone");
 
+        if(!bigCharone){
+            bigCharone = document.createElement("div");
+            bigCharone.id = "bigCharone";
+            bigCharone.classList.add("bigCharone");
+            document.body.appendChild(bigCharone);
+        }
+
+        bigCharone.innerHTML = `<img src="${characters[key]}" alt="">`;
+    })
+    
     box.appendChild(img);
     container.appendChild(box);
 }
 
+// idhar ik loop chal raha he second wale boxContainer1 pe
 
 let mycontainer = document.getElementById("boxContainer1");
 
 for (let key in characters) {
     let box = document.createElement("div");
     box.classList.add("box");
-
+    
     let img = document.createElement("img");
     img.src = characters[key];
 
+    img.addEventListener("click", function(){
+        let bigCharsec = document.getElementById("bigCharsec");
+
+        if(!bigCharsec){
+            bigCharsec = document.createElement("div");
+            bigCharsec.id = "bigCharone";
+            bigCharsec.classList.add("bigCharone");
+            document.body.appendChild(bigCharsec);
+        }
+
+        bigCharsec.innerHTML = `<img src="${characters[key]}" alt="">`;
+    })
+    
     box.appendChild(img);
     mycontainer.appendChild(box);
 }
+
+}, 3000);
+
+
